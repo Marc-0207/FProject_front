9048ed2ff15b3264b3357fd7216ff22ecf05b0b0
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-08-04 09:49:43 +0200

    Organización
    Creadas carpetas y cambiados exports

---
+++ src/components/Auth/Welcome.jsx
@@ -0,0 +1,12 @@
+
+function Welcome(){
+
+    return (
+        <>
+            <h1>Welcome</h1>
+            <button>Login</button>
+            <button>Register</button>
+        </>
+    )
+}
+export default Welcome
